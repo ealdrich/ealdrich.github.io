9f0e3d5eb2500f7ff0683fd90337c26c507f9baa
Eric Aldrich <ealdrich@ucsc.edu>
2014-01-02 20:48:11 -0800

    A third attempt at javascript paths.

--- Teaching/teachingMenu.js
+++ Teaching/teachingMenu.js
@@ -1,3 +1,3 @@
 document.write('<div class="subMenu">')
-document.write('<span class="mainMenu"><a class="mainMenu" href="/~ealdrich/Teaching/Econ133/">ECON 133</a></span>')
+document.write('<span class="mainMenu"><a class="mainMenu" href="ealdrich.github.io/Teaching/Econ133/">ECON 133</a></span>')
 document.write('</div>')
