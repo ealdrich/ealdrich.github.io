f74d18cbe087d6323f6d2a06fb4e11ce8a095887
Eric Aldrich <ealdrich@unix2.lt.ucsc.edu>
2014-01-05 08:56:12 -0800

    Fixed error in rootDir.js in Teaching/Econ133.

--- Teaching/Econ133/rootDir.js
+++ Teaching/Econ133/rootDir.js
@@ -1,9 +1,9 @@
 switch (document.location.hostname)
     {
     case 'people.ucsc.edu':
-	var rootFolder = '/~ealdrich/'; break;
+	var rootFolder = '/~ealdrich/Teaching/Econ133/'; break;
     case 'ealdrich.github.io' :
 	var rootFolder = '/'; break;
     default :
-	var rootFolder = '~/ealdrich.github.io/'; break;
+	var rootFolder = '~/ealdrich.github.io/Teaching/Econ133/'; break;
     }
