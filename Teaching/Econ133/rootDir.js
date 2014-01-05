a834cc5744cc0871b8ead8856b986dea57059269
Eric Aldrich <ealdrich@unix2.lt.ucsc.edu>
2014-01-05 09:19:38 -0800

    One more change to rootDir.js in Teaching/Econ133.

--- Teaching/Econ133/rootDir.js
+++ Teaching/Econ133/rootDir.js
@@ -3,7 +3,7 @@ switch (document.location.hostname)
     case 'people.ucsc.edu':
 	var rootFolder = '/~ealdrich/Teaching/Econ133/'; break;
     case 'ealdrich.github.io' :
-	var rootFolder = '/'; break;
+	var rootFolder = '/Teaching/Econ133/'; break;
     default :
 	var rootFolder = '~/ealdrich.github.io/Teaching/Econ133/'; break;
     }
