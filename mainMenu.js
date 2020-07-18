0b499897724584511c1fefef6b5baec99536d96d
Eric Aldrich <ealdrich@gmail.com>
2020-07-18 12:39:26 -0700

    Update mainMenu.js

--- mainMenu.js
+++ mainMenu.js
@@ -7,4 +7,4 @@ document.write('<span class="mainMenu"><a class="mainMenu" href="'+rootFolder+'R
 document.write('<span class="mainMenu"><a class="mainMenu" href="'+rootFolder+'Teaching/">TEACHING</a></span>')
 document.write('</div>')
 document.write('</div>')
-document.write('<div id="banner"><img id="banner" class="top" src="Images/background_devon.jpg" /><img id="banner" class="bottom" src="Images/background_lugano.jpg" /></div>')
+document.write('<div id="banner"><img id="banner" src="Images/background_devon.jpg" /><img id="banner" src="Images/background_lugano.jpg" /><img id="banner" src="Images/background_lakessun.jpg" /><img id="banner" src="Images/background_lugano2.jpg" /></div>')
