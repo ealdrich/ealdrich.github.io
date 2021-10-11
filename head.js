c7ef3bce19fd8086cf18defc4e1cb18ed1c56aaf
Eric Aldrich <ealdrich@gmail.com>
2021-10-11 06:58:22 -0400

    Update head.js

--- head.js
+++ head.js
@@ -1,7 +1,7 @@
 document.write('<head>')
 document.write('<title>Eric Aldrich</title>')
 document.write('<meta name="description" content="Homepage for Eric M. Aldrich" />')
-document.write('<meta name="keywords" content="Eric Aldrich, Eric M. Aldrich, Economics, University of California, Santa Cruz, Eric Mark Aldrich, Aldrich, Eric" />')
+document.write('<meta name="keywords" content="Eric Aldrich, Eric M. Aldrich, Economics, Amazon, Figure Technologies, University of California, Santa Cruz, Eric Mark Aldrich, Aldrich, Eric" />')
 document.write('<link rel="stylesheet" type="text/css" href="'+rootFolder+'style.css" />')
 document.write('<link href="https://fonts.googleapis.com/css?family=Neuton|News+Cycle|PT+Serif|Varela" rel="stylesheet">')
 document.write('</head>')
